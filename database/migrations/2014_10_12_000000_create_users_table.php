b87f5dfdd6854dc7128252cc5bf70b988c4c25d3
Liantsoamiarina <Liantsoamiarina14@gmail.com>
2025-06-10 16:14:52 +0300

    "Table Acte"

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->unsignedBigInteger("role_id")->nullable();
+            $table->unsignedBigInteger("role_id")->default(1)->nullable();
             $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade')->onUpdate('cascade');
             $table->rememberToken();
             $table->timestamps();
